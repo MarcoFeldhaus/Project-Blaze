e5457c170dde3fb0b5272333f727eb1e1308cf60
Nnaut <theriger1994@hotmail.com>
2015-05-10 22:28:13 +1000

    Update 10/05/2015 10:26pm
    Updated About Us page, implemented shared preferences to map type and
    update interval, Fixed register sqlite(Had to roll back), need to do
    acknowledgements page with Pindrop marker only used. Tidy up code

---
+++ PB_mh_jmcd/app/src/main/java/com/mh_jmcdexample/pb_mh_jmcd/SettingsFragActivity.java
@@ -0,0 +1,113 @@
+package com.mh_jmcdexample.pb_mh_jmcd;
+
+/**
+ * Created by Michael on 7/04/15.
+ */
+import android.app.Activity;
+
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import android.support.v4.app.Fragment;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SettingsFragActivity extends Fragment {
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public static SettingsFragActivity newInstance(int sectionNumber) {
+        SettingsFragActivity fragment = new SettingsFragActivity();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    public SettingsFragActivity() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_settings, container,
+                false);
+
+        return rootView;
+    }
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainHomeFragActivity) activity).onSectionAttached(getArguments().getInt(
+                ARG_SECTION_NUMBER));
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        if (savedInstanceState == null){
+
+            //getActivity().getSupportFragmentManager().beginTransaction()
+
+            //Fragment newFragment = InfoFragment.newInstance(0);
+
+
+
+
+           // getActivity().getFragmentManager().beginTransaction()
+             //       .add(R.id.container, new MyPreferenceFragment())
+               //     .commit();
+
+            //Fragment newFragment = new MyPreferenceFragment();
+
+
+
+
+
+
+
+        }
+
+
+
+
+       // getFragmentManager().beginTransaction()
+       // getActivity().getFragmentManager().beginTransaction()
+
+                //replace R.id.Container does the Preferences on About Page
+                //replace android.R.id.content
+                        //  .replace(android.R.id.content, new MyPreferenceFragment())
+                //.addToBackStack(null)
+                        // .commit();
+
+
+    }
+
+    /*
+
+    public class MyPreferenceFragment extends PreferenceFragment {
+        public MyPreferenceFragment(){}
+
+        @Override
+        public void onCreate (final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.preferences);
+        }
+
+
+    }
+*/
+
+    //getActivity().getFragmentManager().beginTransaction().remove(SettingsFragActivity.class).commit();
+
+}
